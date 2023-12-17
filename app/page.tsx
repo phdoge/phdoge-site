2a284965ec881bf8fa02d9bb678f983f106780a4
Dirk <brigadirk@proton.me>
2023-12-17 19:35:20 +0100

    PHDoge logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
-  title: "PhDoge",
+  title: "PhDoge: The Philosophy Dogster",
   twitter: {
     card: "summary_large_image",
   },
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png",
+        url: "images/phdoge-logo.png",
       },
     ],
   },
